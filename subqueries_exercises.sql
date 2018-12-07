ea782e82d083ef90d2f6f2e52af30657c7e6ad8f
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-12-07 09:02:32 -0600

    redid exercise for subqueries

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -4,18 +4,38 @@ SELECT * FROM employees;
 # Find all the employees with the same hire date as employee 101010 using a sub-query.
 # 69 Rows
 
-SELECT emp_no, hire_date
-FROM employees
-WHERE emp_no IN (
-  SELECT emp_no
-  FROM dept_emp
-  WHERE emp_no LIKE '101010'
-);
+# SELECT CONCAT(first_name, ' ', last_name),hire_date
+# FROM employees
+# WHERE hire_date IN (
+#     SELECT hire_date
+#     FROM employees
+#     WHERE emp_no = 101010
+#     );
+
+select * from employees
+where hire_date IN (
+   SELECT HIRE_DATE
+   FROM employees
+   WHERE emp_no = '101010');
 
 
+select * from titles;
 # Find all the titles held by all employees with the first name Aamod.
 # 314 total titles, 6 unique titles
 
+select * from titles
+where emp_no IN (
+    select emp_no
+    from employees
+    where first_name = 'Aamod'
+    );
 
 # Find all the current department managers that are female.
+
+select first_name, last_name from employees
+where emp_no IN (
+select emp_no from dept_manager
+where to_date > now()
+                )
+  and gender = 'f';
 
